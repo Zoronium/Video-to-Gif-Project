39b454a0d8c01c18e2253c0be458352ba874dbf3
Advait Jain <aj.advaitj@gmail.com>
2023-01-17 13:53:42 +0530

    Feat: added the video to gif funtionality
    - added the functionality using moviepy

---
+++ vidtogig.py
@@ -0,0 +1,10 @@
+from moviepy.editor import VideoFileClip, ImageSequenceClip
+
+def video_to_gif(filepath, duration):
+    clip = VideoFileClip(filepath).subclip(0,duration)
+    gif_filepath = filepath.replace(".mp4", ".gif")
+    clip.write_gif(gif_filepath)
+
+
+if __name__ == "__main__":
+    video_to_gif("example.mp4", 5)
