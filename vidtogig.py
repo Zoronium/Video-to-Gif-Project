3793bb0292c25cd20b086a6501f82bdde31551eb
Advait Jain <aj.advaitj@gmail.com>
2023-01-18 15:08:52 +0530

    Bug: Added Expeyions for incorrect inputs
    - made eception for wrong duration and file type

--- vidtogig.py
+++ vidtogig.py
@@ -1,11 +1,18 @@
 from moviepy.editor import VideoFileClip, ImageSequenceClip
-
+import os.path as p
 def video_to_gif(filepath, duration):
-    clip = VideoFileClip(filepath).subclip(0,duration)
-    gif_filepath = filepath.replace(".mp4", ".gif")
-    clip.write_gif(gif_filepath)
-    clip.close()
+    try:
+        if duration<0:
+            raise ValueError("Duration should be positive")
+        
+        clip = VideoFileClip(filepath).subclip(0,duration)
+        gif_filepath = filepath.replace(".mp4", ".gif")
+        clip.write_gif(gif_filepath)
+        clip.close()
 
+        return gif_filepath
+    except ValueError:
+        return 'Invalid Duaration'
 
 if __name__ == "__main__":
-    video_to_gif("example.mp4", 5)
+    video_to_gif("example.mp4", -1)
